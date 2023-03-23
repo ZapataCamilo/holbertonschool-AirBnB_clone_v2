b103ae67b19897bef930486a8bb29aec1204186f
root <root@DESKTOP-ISQ43BP>
2023-03-23 11:18:09 -0500

    upgrade

--- models/review.py
+++ models/review.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from os import getenv
-
-env = getenv('HBNB_TYPE_STORAGE')
+from models.base_model import BaseModel
 
 
-class Review(BaseModel, Base if (env == "db") else object):
+class Review(BaseModel):
     """ Review classto store review information """
-    if env == "db":
-        __tablename__ = 'reviews'
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    place_id = ""
+    user_id = ""
+    text = ""
