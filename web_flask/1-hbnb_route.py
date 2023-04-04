a20b2521276e2b630b31ca42e2133c642388511b
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-04 11:33:17 -0500

    Task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Import Flask """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Script that starts a Flask web application
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    Script that starts a Flask web application
+    """
+    return 'HBNB' 
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
