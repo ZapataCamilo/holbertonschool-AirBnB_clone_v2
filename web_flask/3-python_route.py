f629ebd485e47b64646ad97df335ef4126e826ea
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-04 16:22:01 -0500

    Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Import Flask """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Script that starts a Flask web application
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    Script that starts a Flask web application
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<string:username>')
+def show_user_profile(username):
+    """
+    display “C ” followed by the value of the text variable
+    """
+    # show the user profile for that user
+    for i in username:
+        if i == '_':
+            return f"C {username.replace('_', ' ')}"
+    return f'C {username}'
+
+
+
+@app.route('/python', defaults={'text' : 'is cool'})
+@app.route('/python/', defaults={'text' : 'is cool'})
+@app.route('/python/<text>')
+def txt(text='is cool'):
+    """
+    script that starts a Flask web application:
+    """
+    for i in text:
+        if i == '_':
+            txt_r = text.replace('_', ' ')
+            return f"Python {txt_r}"
+    return f"Python {text}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
