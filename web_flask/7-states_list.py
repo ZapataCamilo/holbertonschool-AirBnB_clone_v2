977f411951a3b1fb6752a210036ac1eee56a5462
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-06 11:41:33 -0500

    Task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def index():
+def st_list():
     stg = storage.all(State).values()
     return render_template('7-states_list.html', stg=stg)
 
