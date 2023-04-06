31bba80f35527d9f1d27bfb3dad4ab901aaae9b0
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-06 11:50:30 -0500

    Task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-""" Import Flask """
+''' starts a Flask web application'''
 from flask import Flask, render_template
 from models.state import State
 from models import storage
+
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def st_list():
-    """Displays a HTML page with a list of states"""
-    stg = storage.all(State).values()
-    return render_template('7-states_list.html', stg=stg)
+    st = storage.all(State).values()
+    return render_template('7-states_list.html', st=st)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Remove the current SQLAlchemy Session"""
+    '''remove the current SQLAlchemy Session'''
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
