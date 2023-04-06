17e682dd3345cc153c17c5327bcc34a9d20fd2bf
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-06 13:22:40 -0500

    Task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+''' starts a Flask web application'''
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<path:id>', strict_slashes=False)
+def path_st_list(id):
+    st = storage.all(State).values()
+
+    if not id:
+        return render_template('7-states_list.html', items=st)
+    
+    r_st = 'State.{}'.format(id)
+    if r_st in st:
+        return render_template(
+            '9-states.html',
+            st='State" {}'.format(st[r_st].name),
+            items=st[r_st])
+
+    return render_template('9-states.html', item=None)
+
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    '''remove the current SQLAlchemy Session'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
